303fa50f466e8054b393425907a3712120cb0a33
Maks <max@moais.ru>
2023-01-25 18:41:53 +0300

    task1-4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,56 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        task1();
+        task2_3();
+        task4();
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        int[] arrCosts = generateRandomArray();
+        int totalCost = 0;
+        for (int elem : arrCosts){
+            totalCost += elem;
+        }
+        System.out.println("Сумма трат за месяц составила " + totalCost + " рублей");
+    }
+
+    public static void task2_3() {
+        int[] arrCosts = generateRandomArray();
+        int minCost = arrCosts[0], maxCost = arrCosts[0];
+        double totalCost = 0.0;
+
+        for (int elem : arrCosts){
+            if(minCost > elem){
+                minCost = elem;
+            }
+            if(maxCost < elem){
+                maxCost = elem;
+            }
+
+            totalCost += elem;
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minCost + " рублей. Максимальная сумма трат за день составила " + maxCost + " рублей");
+        System.out.printf("Средняя сумма трат за месяц составила %.2f рублей", totalCost/arrCosts.length);
+    }
+
+    public static void task4() {
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        int lengthArray = reverseFullName.length;
+
+        System.out.println();
+        for (int i = (lengthArray - 1);  i >= 0; i--){
+            System.out.print(reverseFullName[i]);
+        }
+
     }
 }
